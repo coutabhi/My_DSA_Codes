b732435c682161b85ca97dbd7d1c0f0cc2e989cf
Abhishek Yadav <abhi0905yadav@gmail.com>
2023-01-16 22:39:51 +0530

    added

---
+++ My_Codes/29_OperatorOverloading1.cpp
@@ -0,0 +1,16 @@
+//Code by Abhishek Yadav👨🏻‍💻
+//Operator overloading I
+
+#include<iostream>
+using namespace std;
+//Operator oveloading is also known as polymorphism.
+
+class Fraction{
+    int numerator;
+    int denominator;
+ Fraction(){
+    
+};
+int main(){
+    return 0;
+}
